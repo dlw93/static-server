7632a1cbb2b807df3d58787e59e6600425653e98
David Luis Wiegandt <contact@dlw.dev>
2022-09-28 17:14:06 +0200

    minor fixes

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,45 @@
-import { readFile } from "fs";
-import { createServer, IncomingMessage, OutgoingHttpHeaders, ServerResponse } from "http";
-import { extname, resolve } from "path";
+import {readFile} from "fs";
+import {createServer, IncomingMessage, OutgoingHttpHeaders, ServerResponse} from "http";
+import {resolve, join} from "path";
 
+import {lookup as mimeLookup} from "mime-types";
 import yargs from "yargs";
-import { lookup } from "mime-types";
 
-const { port, baseDir, cors } = yargs(process.argv.slice(2))
+const [, scriptName, ...argv] = process.argv;
+const {port, baseDir, cors} = yargs(argv)
+    .scriptName(scriptName)
     .options({
-        "port": { alias: "p", type: "number", default: 8081, description: "The port to listen on" },
-        "base-dir": { alias: "d", type: "string", default: "/public", description: "The directory to serve content from" },
-        "cors": { alias: "c", type: "boolean", default: true, description: "Whether to enable CORS" },
+        "port": {
+            alias: "p",
+            type: "number",
+            default: 8081,
+            description: "The port to listen on"
+        },
+        "base-dir": {
+            alias: "d",
+            type: "string",
+            default: ".",
+            description: "The directory to serve content from",
+            normalize: true
+        },
+        "cors": {
+            alias: "c",
+            type: "boolean",
+            default: true,
+            description: "Whether to enable CORS"
+        },
     })
-    .locale("en-US")
+    .locale("en")
     .parseSync();
 
-const corsHeader = cors ? { "Access-Control-Allow-Origin": "*" } : {};
+const corsHeader = cors ? {"Access-Control-Allow-Origin": "*"} : {};
 
 function handler(request: IncomingMessage, response: ServerResponse) {
-    console.log(`Request ${request.url}`);
+    const targetFilePath = resolve(join(baseDir, request.url!));
 
-    readFile(`.${baseDir}${request.url}`, (error, content) => {
+    console.log(`Request: ${request.url} -> ${targetFilePath}`);
+
+    readFile(`${baseDir}${request.url}`, (error, content) => {
         if (error) {
             if (error.code === "ENOENT") {
                 response.writeHead(404).end("Not Found");
@@ -28,7 +48,7 @@ function handler(request: IncomingMessage, response: ServerResponse) {
             }
         } else {
             const headers: OutgoingHttpHeaders = {
-                "Content-Type": lookup(extname(request.url!)) || "application/octet-stream",
+                "Content-Type": mimeLookup(request.url!) || "application/octet-stream",
                 ...corsHeader,
             }
             response.writeHead(200, headers).end(content, "utf-8");
@@ -39,4 +59,4 @@ function handler(request: IncomingMessage, response: ServerResponse) {
 createServer(handler).listen(port);
 
 console.log(`Listening on http://127.0.0.1:${port}`);
-console.log(`Serving content from ${resolve("." + baseDir)}`)
+console.log(`Serving content from ${resolve(baseDir)}`);
